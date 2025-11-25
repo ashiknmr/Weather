9fc7919c15c4ae6e90d3f78fe22a51f645ed3a10
Ashik <ashiknmr2000@gmail.com>
2025-11-25 21:04:16 +0530

    reworked on some elements

--- script.js
+++ script.js
@@ -7,30 +7,42 @@ document.querySelector("button").addEventListener("click", () => {
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
         .then(res => {
-            if (!res.ok) {
-                throw new Error("City not found");
-            }
+            if (!res.ok) throw new Error("City not found");
             return res.json();
         })
-        .then(data => {
+        .then(async data => {
+
+            // WEATHER DATA
             document.querySelector(".title").innerHTML = `Weather in ${data.name}`;
             document.querySelector(".temp").innerHTML = `${data.main.temp}°C`;
-            const description = document.querySelector(".description");
-            description.innerHTML = data.weather[0].description;
-            document.querySelector(".weather-icon").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+            document.querySelector(".description").innerHTML = data.weather[0].description;
+            document.querySelector(".weather-icon").src = 
+                `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
 
             document.querySelector(".box:nth-child(1)").innerHTML = `Feels like: ${data.main.feels_like}`;
             document.querySelector(".box:nth-child(2)").innerHTML = `Humidity: ${data.main.humidity}%`;
             document.querySelector(".box:nth-child(3)").innerHTML = `Wind speed: ${data.wind.speed} m/s`;
-            document.querySelector(".box:nth-child(4)").innerHTML = `Temprature min: ${data.main.temp_min}°C`;
-            document.querySelector(".box:nth-child(5)").innerHTML = `Temprature Max: ${data.main.temp_max}°C`;
+            document.querySelector(".box:nth-child(4)").innerHTML = `Temperature Min: ${data.main.temp_min}°C`;
+            document.querySelector(".box:nth-child(5)").innerHTML = `Temperature Max: ${data.main.temp_max}°C`;
             document.querySelector(".box:nth-child(6)").innerHTML = `Pressure: ${data.main.pressure} p`;
-            document.querySelector(".box:nth-child(7)").innerHTML = `Sea level: ${data.main.sea_level ? data.main.sea_level + ' MSL' : 'N/A'}`;
-            document.querySelector(".box:nth-child(8)").innerHTML = `Ground level: ${data.main.grnd_level ? data.main.grnd_level : 'N/A'}`;
+
+
+            const lat = data.coord.lat;
+            const lon = data.coord.lon;
+
+            const timeResponse = await fetch(
+                `https://timeapi.io/api/Time/current/coordinate?latitude=${lat}&longitude=${lon}`
+            );
+
+            const timeData = await timeResponse.json();
+
+            document.querySelector(".date").innerHTML = `Date: ${timeData.date}`;
+            document.querySelector(".time").innerHTML = `Time: ${timeData.time}`;
+            
             document.getElementById("cityInput").value = "";
         })
         .catch(() => {
-             showToast("⚠️ City not found! Please try again.");
+            showToast("⚠️ City not found! Please try again.");
         });
 });
 
@@ -39,7 +51,6 @@ function showToast(message) {
     toast.innerText = message;
     toast.style.bottom = "150px";
     toast.style.opacity = "1";
-
     setTimeout(hideToast, 3000);
 }
 function hideToast() {
